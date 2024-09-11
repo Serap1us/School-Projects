be5618d8b98549e1a48b06248bd78b50bb111747
Serap1us <markmandac07@gmail.com>
2024-09-10 23:14:55 -0600

    Fixed offset
    player is now centred properly

--- super-hexiclone/obstacle.gd
+++ super-hexiclone/obstacle.gd
@@ -1,10 +1,13 @@
 extends AnimatableBody2D
 class_name Obstacle
 @onready var Shape: CollisionPolygon2D = $CollisionPolygon2D
-var shrinkSpeed = 10.0
+var shrinkSpeed = 8.0
 
-func _ready():
+func _ready():					
 	Shape.scale = Vector2.ONE*10.0
+	
+	# Randomize Initial Rotation
+	rotation = randf_range(0, 2* PI)
 	
 func _physics_process(delta: float) -> void:
 	Shape.scale -= Vector2.ONE * shrinkSpeed * delta
